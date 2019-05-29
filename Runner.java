9293f934249d722b162696f9e9b091197b0dda32
Mihir Laud <mihirlaud@gmail.com>
2019-05-29 10:29:00 -0400

    Combined all work into one single file

--- Runner.java
+++ Runner.java
@@ -2,7 +2,27 @@ public class Runner {
 
 	public static void main(String[]args) {
 	
-	
+		PrintQuotient p = (float a, float b) -> {
+		
+			System.out.printf("%.3f", a / b);
+		
+		};
+		
+		p.printQuotient(10.0f, 3.0f);
+		
+		PerfectSquare printer = (int number) -> {
+
+				int x = (int) Math.sqrt(number);
+				if(Math.pow(x,2) == number)
+					return true;
+				else
+					return false;
+
+
+
+		};
+
+		System.out.println(printer.PerfectSquare(25));
 	
 	}
 
