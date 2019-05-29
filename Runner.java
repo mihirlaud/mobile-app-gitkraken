47a8386c9e85bcdbbf8da498739c612e04da088c
Cory Petersen <cnpkiwi07@gmail.com>
2019-05-29 10:34:27 -0400

    Partner B added comments about what he/she learned

--- Runner.java
+++ Runner.java
@@ -1,15 +1,18 @@
 public class Runner {
 
 	public static void main(String[]args) {
-	
+
+		//Cory: I learned how to operate GitKraken. This is my first time using GitHub and GitKraken. I learned how to create and merge branches
+		//		as well as how to write code in Lambda notation.
+
 		PrintQuotient p = (float a, float b) -> {
-		
+
 			System.out.printf("%.3f", a / b);
-		
+
 		};
-		
+
 		p.printQuotient(10.0f, 3.0f);
-		
+
 		PerfectSquare printer = (int number) -> {
 
 				int x = (int) Math.sqrt(number);
@@ -23,7 +26,7 @@ public class Runner {
 		};
 
 		System.out.println(printer.PerfectSquare(25));
-	
+
 	}
 
 }
